11ac0c09127bb3620c069336940501363452b41e
Lars Gentsch <lars.gentsch@gmail.com>
2023-11-12 14:43:24 +0100

    Updated Maven dependencies.

--- src/main/java/de/thb/demo/HelloController.java
+++ src/main/java/de/thb/demo/HelloController.java
@@ -1,11 +1,13 @@
 package de.thb.demo;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Value;
 
 @RestController
 public class HelloController {
+
 	@Value("${spring.application.name}")
 	String name;
 
@@ -14,4 +16,9 @@ public class HelloController {
 		return "Greetings from " + name + "!";
 	}
 
+
+	@GetMapping("/something")
+	public ResponseEntity<String> createLogs() {
+		return ResponseEntity.ok().body("All Ok");
+	}
 }
