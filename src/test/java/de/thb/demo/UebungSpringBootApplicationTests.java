db54dadc74a8d0943820a265872ceab732af848f
cdwb1 <cdwb11@gmail.com>
2023-12-01 13:31:56 +0100

    added test

--- src/test/java/de/thb/demo/UebungSpringBootApplicationTests.java
+++ src/test/java/de/thb/demo/UebungSpringBootApplicationTests.java
@@ -4,11 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 class UebungSpringBootApplicationTests {
-
 	@Test
 	void contextLoads() {
 		int x = 0;
-		int y = 1;
+		int y = 1+1;
 		System.out.println("This is a unit test!");
 		assert x != y;
 	}
